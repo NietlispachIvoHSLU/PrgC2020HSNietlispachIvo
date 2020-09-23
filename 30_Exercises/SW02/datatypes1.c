c7c4fcd69d746129b9055ce4a700d5fc053c136c
NietlispachIvoHSLU <ivo.nietlispach@stud.hslu.ch>
2020-09-23 10:12:23 +0200

    checking datatype

---
+++ 30_Exercises/SW02/datatypes1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ( int argc, char* argv[] ) {
+
+	printf("Size of Char is: %ld\n", sizeof(char));
+		printf("Size of Short is: %ld\n", sizeof(short));
+	printf("Size of Int is: %ld\n", sizeof(int));
+	printf("Size of Long is: %ld\n", sizeof(long));
+	printf("Size of long long is: %ld\n", sizeof(long long));
+
+	return 0;
+}
+
